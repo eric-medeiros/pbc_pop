434dee6d838e6ac5ecdeea87cef0b7ec1a507a82
Eric Medeiros <ericmedeiros@gmail.com>
2023-04-14 15:43:02 -0300

    script para instalação dos pacotes necessários

--- 01_scripts/0_install_libs.R
+++ 01_scripts/0_install_libs.R
@@ -1,13 +1,12 @@
 rm(list = ls())
 
-lista_funcs <- list.files(paste0(rprojroot::find_rstudio_root_file(), "/01_scripts"), "*.R", full.names = TRUE)
+lista_funcs <- list.files(paste0(rprojroot::find_rstudio_root_file(), "/01_scripts"), "*.R", full.names = TRUE)[-1]
 
 funcs <- suppressWarnings(unlist(lapply(lista_funcs, readLines)))
 
-lines_libs <- funcs[regexpr("library", funcs)!=-1]
+lib_lines <- funcs[regexpr("library", funcs)!=-1]
 
-names_libs <- gsub("*library\\(", "", lines_libs)
-names_libs <- gsub("\\)", "", names_libs) 
-names_libs <- gsub(" ", "", names_libs) 
+names_libs <- unique(gsub("*library\\(|\\)| ", "", lib_lines))
 
-install.packages(unique(names_libs))
+update.packages(names_libs)
+install.packages(names_libs)
