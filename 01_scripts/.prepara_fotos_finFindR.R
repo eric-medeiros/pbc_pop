b088dab85a5186e3d6cbe850928a290b81bdbdc2
Eric Medeiros <ericmedeiros@gmail.com>
2023-05-08 17:21:18 -0300

    prepara uma pasta com fotos do catálogo e uma planilha com arquivo e ID

--- 01_scripts/.prepara_fotos_finFindR.R
+++ 01_scripts/.prepara_fotos_finFindR.R
@@ -1,12 +1,14 @@
+library(dplyr)
+library(stringr)
 
 pasta_cat <- "//NAS_IPeC/PBC-Pesquisa/PROJETOS/ANDAMENTO/01_FOTOID/02_ANALISE/01_HIST_ID"
 
 caminho_IDs <- list.files(pasta_cat, full.names = TRUE)
 
 tab_ID <- function(caminho_ID){
-  tibble(arquivo = list.files(caminho_ID, ".JPG", ignore.case = TRUE),
+  tibble(Image = list.files(caminho_ID, ".JPG", ignore.case = TRUE),
          caminho = list.files(caminho_ID, ".JPG", ignore.case = TRUE, full.names = TRUE),
-         ID = str_sub(caminho_ID, -5, -1))
+         CatalogID = str_sub(caminho_ID, -5, -1))
 }
 
 tab_IDs <- lapply(caminho_IDs, tab_ID) %>% bind_rows()
@@ -19,6 +21,6 @@ caminho_csv_saida <- paste0(caminho_pasta_saida, "/IDs.csv")
 #Salva CSV
 file.copy(tab_IDs$caminho, caminho_pasta_saida, overwrite = FALSE, copy.date = TRUE)
 #Salve cópia das fotos 
-write.csv(tab_IDs[c("ID", "arquivo")], caminho_saida_csv, row.names = FALSE)
+write.csv(tab_IDs[c("Image", "CatalogID")], caminho_csv_saida, row.names = FALSE)
 
 
