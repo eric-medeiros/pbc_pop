1118b50ed110f8eaf13c805688ad2231b234fd88
Eric Medeiros <ericmedeiros@gmail.com>
2025-06-20 09:44:47 -0300

    aesthetics for post

--- 01_scripts/bd_sub_wps.R
+++ 01_scripts/bd_sub_wps.R
@@ -6,31 +6,50 @@ bd_sub_wps <- function(dados_excel, dados_wps) {
   
   # Juntando coordenadas inseridas manualmente na planilha com pontos dos GPS
   # Preparando os dados wp_sub
-  dados_wps_tudo <- dados_excel$wps_extra %>%
-    transmute(saida = saida,
-              datahora = datahora_extra,
-              wp = wp_extra,
-              lng = as.double(lng_extra),  # Convertendo para double
-              lat = as.double(lat_extra)) %>%
+  dados_wps_tudo <-
+    dados_excel$wps_extra %>%
+    transmute(
+      saida = saida,
+      datahora = datahora_extra,
+      wp = wp_extra,
+      lng = as.double(lng_extra),
+      lat = as.double(lat_extra)
+    ) %>%
     bind_rows(dados_wps) %>%
     arrange(datahora)
   
-  wp_sub <- dados_wps_tudo %>%
+  wp_sub <-
+    dados_wps_tudo %>%
     select(saida, datahora, wp, lng, lat)
   
   # Função para atualizar coordenadas genérica
   atualiza_coordenadas_generico <- function(dados, wp_sub, tipo) {
-    dados[[tipo]] <- dados[[tipo]] %>%
+    dados[[tipo]] <-
+      dados[[tipo]] %>%
       left_join(wp_sub, by = c("saida", "wp_i" = "wp")) %>%
       rename(wp_I = wp_i) %>%
-      dplyr::select(1:wp_I, datahora_I = datahora, lng_I = lng, lat_I = lat, wp_I:last_col()) %>%
+      dplyr::select(
+        1:wp_I,
+        datahora_I = datahora,
+        lng_I = lng,
+        lat_I = lat,
+        wp_I:last_col()
+      ) %>%
       left_join(wp_sub, by = c("saida", "wp_f" = "wp")) %>%
       rename(wp_F = wp_f) %>%
-      dplyr::select(1:wp_F, datahora_F = datahora, lng_F = lng, lat_F = lat, wp_I:last_col()) %>%
-      mutate(lng_I = round(lng_I, 5),
-             lat_I = round(lat_I, 5),
-             lng_F = round(lng_F, 5),
-             lat_F = round(lat_F, 5))
+      dplyr::select(
+        1:wp_F,
+        datahora_F = datahora,
+        lng_F = lng,
+        lat_F = lat,
+        wp_I:last_col()
+      ) %>%
+      mutate(
+        lng_I = round(lng_I, 5),
+        lat_I = round(lat_I, 5),
+        lng_F = round(lng_F, 5),
+        lat_F = round(lat_F, 5)
+      )
     
     return(dados)
   }
@@ -42,14 +61,17 @@ bd_sub_wps <- function(dados_excel, dados_wps) {
   }
   
   # Atualizar dados de caminhos
-  dados_excel$caminhos <- dados_wps_tudo %>%
+  dados_excel$caminhos <-
+    dados_wps_tudo %>%
     select(-lng, -lat, -datahora) %>%
     filter(arquivo_wp != "NA") %>%
     nest(id = c(saida, wp)) %>%
-    transmute(tipo = "gps_wp",
-              id = id,
-              arquivo = basename(arquivo_wp),
-              pasta = dirname(arquivo_wp)) %>%
+    transmute(
+      tipo = "gps_wp",
+      id = id,
+      arquivo = basename(arquivo_wp),
+      pasta = dirname(arquivo_wp)
+    ) %>%
     bind_rows(dados_excel$caminhos)
   
   #Adicionar wps ao banco
@@ -62,3 +84,6 @@ bd_sub_wps <- function(dados_excel, dados_wps) {
   
   return(dados_excel)
 }
+
+
+
